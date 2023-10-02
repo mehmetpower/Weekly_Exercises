1e0527870a176b4ccc36388cb826da144fa6696a
powermehmet <mehmet.avci@powercoders.org>
2023-10-02 17:37:22 +0200

    update exercise 4

--- Week_3/Day_1/exercise_4.py
+++ Week_3/Day_1/exercise_4.py
@@ -1,24 +1,20 @@
+check_list = [31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+empty_list = []
+
 months =	{
   "1": 31, "2": 28, "3": 31, "4": 30, "5": 31, "6": 30,
   "7":31, "8": 31, "9": 30, "10": 31, "11": 30, "12": 31
 }
 
-def day_calculator():
-  user_input = input("Enter a key: ")
-  for i, key in enumerate(months.keys()):
-      if key == user_input:
-          index = i
-          print(f"The index of '{user_input}' is {index}") 
- 
-
 def calculator():
-   month = input("Enter a month: ")
-   day = input(f"Enter the day of the month {month}: ")
+   month = int(input("Enter a month: ")) - 1
+   day = input(f"Enter the day of the month {month + 1}: ")
    days_in_months = list(months.values())
    month_sum=0
    for i in range(int(month)):
       month_sum += days_in_months[i]
-      total = month_sum+int(day)
-   print(f"The date you entered is the day #{total} of the year.")
+      empty_list.append(month_sum)
+   print('empty list:', empty_list)
+   print(f"The date you asked is the day #{month_sum+int(day)} of the year.")
 
 calculator()
