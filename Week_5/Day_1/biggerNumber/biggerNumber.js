14ab67943c88b3e57a537b57bd05017beaebaee8
powermehmet <mehmet.avci@powercoders.org>
2023-10-17 00:02:26 +0200

    exercise bigger number

---
+++ Week_5/Day_1/biggerNumber/biggerNumber.js
@@ -0,0 +1,13 @@
+let number1 = 5;
+let number2 = 10;
+
+if (number1 > number2) {
+    console.log(`The greater number of ${number1} and ${number2} is ${number1}.`);
+    console.log(`The smaller number of ${number1} and ${number2} is ${number2}.`);
+} else if (number1 < number2) {
+    console.log(`The greater number of ${number1} and ${number2} is ${number2}.`);
+    console.log(`The smaller number of ${number1} and ${number2} is ${number1}.`);
+} else {
+    console.log(`Both numbers, ${number1} and ${number2}, are equal.`);
+}
+
