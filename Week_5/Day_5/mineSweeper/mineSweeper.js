983265aeca8e256810b1b4db65439ba3658df7ab
powermehmet <mehmet.avci@powercoders.org>
2023-10-20 12:35:40 +0200

    TASK-2

--- Week_5/Day_5/mineSweeper/mineSweeper.js
+++ Week_5/Day_5/mineSweeper/mineSweeper.js
@@ -1,10 +1,11 @@
 
 // Set this constant to true to debug the placement of bombs without
 // having to click on all cells to reveal them.
-const CHEAT_REVEAL_ALL = false;
+const CHEAT_REVEAL_ALL = true;
 
 const ROWS_COUNT = 10;
 const COLS_COUNT = 10;
+const BOMBS_COUNT = 3;
 
 var defeat = false;
 var victory = false;
@@ -27,15 +28,28 @@ for (var row = 0; row < ROWS_COUNT; row++) {
 
 //
 // TODO: Task 1 - add some bombs at fixed positions.
-// cells[0][1].isBomb = true;
-// cells[5][4].isBomb = true;
+// cells[0][0].isBomb = true;
+// cells[1][7].isBomb = true;
+// cells[4][9].isBomb = true;
+// cells[8][2].isBomb = true;
+// cells[9][2].isBomb = true;
 // cells[9][9].isBomb = true;
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+for (let i = 0; i<BOMBS_COUNT; i++){
+  cells[randomNumber(0, 10)][randomNumber(0, 10)].isBomb = true;
+}
+
+
 
 //
 // TODO: Task 2 - Comment out the code of task 1. Instead of adding bombs in fixed places, add 10 of them in random places.
 //                Add a BOMBS_COUNT constant so that you can easily change the amount of bombs placed. Put it next to the
 //                other constants.
 //
+
 
 
 // Once the game has been initialized, we "render" it.
